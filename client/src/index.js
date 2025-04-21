b7ffa4b0bc02c754e062d0caf3d77facb9f91a80
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-04-22 01:08:27 +0530

    Resolved frontend dependency conflict

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Import your main App component
-import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
