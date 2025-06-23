3cb33d08bf37f820f67fdf55023fbbdcf76b5c28
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-06-23 18:26:40 +0530

    updated: improved Navbar styling and layout behavior

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -146,15 +146,6 @@ const Navbar = () => {
             Our Story
           </Link>
 
-          <Link
-            to="/fridge-mate"
-            style={navLinkStyle}
-            onMouseOver={hoverIn}
-            onMouseOut={hoverOut}
-          >
-            Fridge Mate
-          </Link>
-
           {isAuthenticated ? (
             <>
               <Link
